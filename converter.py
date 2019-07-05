06af4a1d9624c4f2c9f2d5b9963c13d12a3cf32d
Sachin Tripathi <sachintripathis84@gmail.com>
2019-07-05 09:51:34 +0530

    Update converter.py

--- converter.py
+++ converter.py
@@ -5,15 +5,40 @@ print"============================================================"
 print "\t\t      Binary Converter"
 print"\t0110100001100101011011000110110001101111"
 print"============================================================"
-decimal = input("Enter the number you want to convert:\n>")
-decimal_o = str(decimal)
-b = []
-while decimal > 0:
-    if decimal%2 == 0:
-        b.append(0)
-    else:
-        b.append(1)
-    decimal= decimal/2
-binary =(b[::-1])
-print "Binary conversion for "+decimal_o+" is "+str(binary)
 
+
+userInput = input()
+
+
+def dec2bin(decimal):
+    decimal = int(decimal)
+    result = ''
+    while decimal > 0:
+        binary = decimal % 2
+        result += str(binary)
+        decimal = decimal//2
+    return result[::-1]
+
+
+def bin2dec(binary):
+    userInput = reversed(str(binary))
+
+    result = 0
+    for i, num in enumerate(userInput):
+        power = 2**i
+        if int(num) == 1:
+            result += power
+        else:
+            result += 0
+
+    return result
+
+
+def numberChecker(number):
+    int(number)
+    for elem in number:
+        if elem == 0 or elem == 1:
+            pass
+
+
+print(dec2bin(userInput))
